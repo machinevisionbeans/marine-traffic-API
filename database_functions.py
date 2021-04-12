5ffe795823c90ad7caa3eb312b1e20a07d9221e6
spykard <kardakis@ceid.upatras.gr>
2021-04-12 20:10:13 +0300

    v1.0 Ready

--- database_functions.py
+++ database_functions.py
@@ -4,38 +4,45 @@ import configparser
 ''' MySQL Database : ------------------------------------------------------------ '''
 '''                  | post_id | shp_owner | latitude | longtitude | excel_copy | '''
 '''                  ------------------------------------------------------------ '''
+''' https://mariadb.com/docs/clients/connector-python/                            '''
 
 def connectDB():
     config = configparser.ConfigParser()
     config.read('config/config.ini')
+    #config.read('/root/my_python_scripts/config/config.ini')    
     # Instantiate Connection
     try:
         conn = mariadb.connect(
             user=config['Settings']['user'],
             password=config['Settings']['password'],
             host=config['Settings']['host'],
-            port=int(config['Settings']['port']))
+            port=int(config['Settings']['port']),
+            database=config['Settings']['database'])
     except mariadb.Error as e:
         print(f"Error connecting to MariaDB Platform: {e}")
         exit(0)
     return (conn)
 
-def setDB(conn):
-    # TEMP CODE
-    cnx = mysql.connector.connect(user='root', password='root', database='newsapp')
-    cursor = cnx.cursor()
-    cursor.execute('SET NAMES utf8mb4')  # Some symbols are in fact UTF8 4 Bit
-    cursor.execute("SET CHARACTER SET utf8mb4")
-    cursor.execute("SET character_set_connection=utf8mb4")
-    query = ("INSERT INTO theguardian VALUES (%s, %s, %s, %s, %s, %s, %s)")
-    cursor.execute(query, (countVariable, textVariable, titleVariable, authorVariable, categoryVariable, dateVariable, urlVariable))
-    cnx.commit()
-    
-    cursor.close()
-    cnx.close()  
+def setDB(conn, ship_id, longitude, latitude, speed, last_auto_update):
+    # Insert Data
+    cursor = conn.cursor()
+
+    try: 
+        if longitude == 0:  # Mode: Update just 1 field
+            cursor.execute("UPDATE iR4muqNI_acf_ships SET last_auto_update = ? WHERE ship_id = ?", (last_auto_update,ship_id)) 
+        else:  # Mode: Update all fields
+            cursor.execute("UPDATE iR4muqNI_acf_ships SET latitude = ?, longitude = ?, speed = ?, last_auto_update = ? WHERE ship_id = ?", (latitude,longitude,speed,last_auto_update,ship_id)) 
+    except mariadb.Error as e: 
+        print(f"Error during the Update Query: {e}")
+    conn.commit()
+
+    print(f"Database Update was run, Affected Rows: {cursor.rowcount}")
 
 def getDB(conn):
-    # GET SHIP NAMES FROM DATASET
-    print("hello")
+    # Retrieve Data
+    cursor = conn.cursor()
 
-print(connectDB())
+    cursor.execute("SELECT ship_id FROM iR4muqNI_acf_ships")
+
+    # Get Result-set
+    return (tuple(cursor))
--- main.py
+++ main.py
@@ -1,38 +1,64 @@
 import database_functions
+import configparser
 from marinetrafficapi import MarineTrafficApi
 from marinetrafficapi import exceptions
 from datetime import datetime
 
-# Connect to DB
-#conn = database_functions.connectDB()
-# Get ship names from DB
-#database_functions.getDB(conn) 
+# Config Parsing
+config = configparser.ConfigParser()
+config.read('config/config.ini')
+#config.read('/root/my_python_scripts/config/config.ini')
 
-testname1 = "hello"
+# Connect to DB
+conn = database_functions.connectDB()
+# Get ship IDs from DB
+ship_ids = database_functions.getDB(conn) 
 
 # Connect to the API
-api = MarineTrafficApi(api_key="56d7d3f632a4565405a7ed674ff5165ef830a31b")
+api = MarineTrafficApi(api_key=config['API']['api_key'])
 # Query the API (https://www.marinetraffic.com/en/ais-api-services/documentation/api-service:ps07)
-try:
-    vessel = api.single_vessel_positions(time_span=20,
-                                        msg_type='simple',
-                                        mmsi=370272000)
-    MarineTrafficApi.print_params_for('single_vessel_positions')
-    vessel = vessel.models[0]
+MarineTrafficApi.print_params_for('single_vessel_positions')
+count_used_credits = int(config['API']['used_credits'])
+for ship_id in ship_ids:
+    print(f"\nRetrieved ship with ID: {ship_id[0]}, now Querying the API...")  
 
-    longitude = vessel.longitude.value
-    latitude = vessel.latitude.value
-    last_auto_update = "Coordinate timestamp is " + str(vessel.timestamp.value) + " UTC"
+    try:
+        vessel = api.single_vessel_positions(time_span=20,
+                                            msg_type='simple',
+                                            mmsi=ship_id[0])
+        vessel = vessel.models[0]
+        print(f"API Run Successfully")
 
-except exceptions.MarineTrafficRequestApiException as e:
-    print(f"API Request Error: {e}")
-    time_now_utc = datetime.utcnow().replace(microsecond=0)
+        longitude = vessel.longitude.value
+        latitude = vessel.latitude.value
+        speed = float(vessel.speed.value)/10
+        last_auto_update = "Coordinate timestamp is " + str(vessel.timestamp.value) + " UTC"
+        count_used_credits += 1
 
-    longitude = 0
-    latitude = 0
-    last_auto_update = "Attempted at " + str(time_now_utc) + " UTC, but failed"
+    except exceptions.MarineTrafficRequestApiException as e:
+        print(f"API Request Error: {e}")
+        time_now_utc = datetime.utcnow().replace(microsecond=0)
 
-print(longitude, latitude, last_auto_update)
+        longitude = 0
+        latitude = 0
+        speed = 0
+        last_auto_update = "Attempted at " + str(time_now_utc) + " UTC, but failed. Vessel not found."
 
-# Update DB
-#database_functions.setDB(conn)
+    except IndexError as e:
+        print(f"API Request Error, ship exists but position is too old: {e}")
+        time_now_utc = datetime.utcnow().replace(microsecond=0)
+
+        longitude = 0
+        latitude = 0
+        speed = 0        
+        last_auto_update = "Attempted at " + str(time_now_utc) + " UTC, but failed. Vessel exists but position is too old"
+
+    # Update DB
+    database_functions.setDB(conn, ship_id[0], longitude, latitude, speed, last_auto_update)
+
+config.set('API', 'used_credits', str(count_used_credits))
+with open('config/config.ini', 'w') as configfile:
+#with open('/root/my_python_scripts/config/config.ini', 'w') as configfile:
+    config.write(configfile)
+
+conn.close()
